cc2d00f046fde3ded96902556383a177f9bf1823
Gabriel Mattos Teixeira dos Santos <129562117+gabrielmatsan@users.noreply.github.com>
2024-11-09 14:34:18 -0300

    fix 🐛 : injeta caso de uso de envio de notificações no manipulador de eventos de retorno de pedido.

--- src/domain/notification/application/subscribers/on-order-returned.ts
+++ src/domain/notification/application/subscribers/on-order-returned.ts
@@ -2,10 +2,11 @@ import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
 import { OrderReturnedEvent } from '@/domain/delivery/enterprise/events/order-returned-event'
 import { Injectable } from '@nestjs/common'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
 
 @Injectable()
 export class OnOrderReturned implements EventHandler {
-  constructor() {
+  constructor(private sendNotification: SendNotificationUseCase) {
     this.setupSubscriptions()
   }
 
@@ -17,6 +18,9 @@ export class OnOrderReturned implements EventHandler {
   }
 
   private async sendNewOrderNotification({ order }: OrderReturnedEvent) {
-    console.log(`Order ${order.id} was returned`)
+    await this.sendNotification.execute({
+      recipientId: order.recipientId.toString(),
+      title: `Order "${order.id}" returned`,
+    })
   }
 }
